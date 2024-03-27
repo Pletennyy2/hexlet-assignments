3a0d2d40e3e4c9b295bef8047c737f885eaf63a4
@hexlet/cli <support@hexlet.io>
2024-03-27 14:16:36 +0300

    submit rails-basics-ru/models

--- rails-basics-ru/models/db/schema.rb
+++ rails-basics-ru/models/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_111422) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_093152) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
