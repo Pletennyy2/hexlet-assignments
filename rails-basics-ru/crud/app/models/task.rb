42933eb759f177cf58895567d310c6f3949a681d
@hexlet/cli <support@hexlet.io>
2024-04-02 15:43:11 +0300

    submit rails-basics-ru/crud

---
+++ rails-basics-ru/crud/app/models/task.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  status      :string
+#  creator     :string
+#  performer   :string
+#  completed   :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :status, presence: true
+  validates :creator, presence: true
+end
