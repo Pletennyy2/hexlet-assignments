44fdd0e5cd12248b780d8b970e6fdee5ace19dbd
@hexlet/cli <support@hexlet.io>
2024-04-04 16:23:42 +0300

    submit rails-basics-ru/forms

---
+++ rails-basics-ru/forms/app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:success] = 'post was successfully updated'
+      redirect_to post_path(@post)
+    else
+      flash[:failure] = 'post cannot be updated'
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:success] = 'post was successfully deleted'
+      redirect_to posts_path
+    else
+      flash[:failure] = 'post cannot be deleted'
+      redirect_to post_path(@post)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :summary, :published)
+  end
+end
