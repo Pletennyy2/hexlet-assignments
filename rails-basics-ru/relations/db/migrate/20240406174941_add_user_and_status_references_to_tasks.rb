1331aacebc0d38d3f331c98d64eb28ae8bd90083
@hexlet/cli <support@hexlet.io>
2024-04-06 21:53:31 +0300

    submit rails-basics-ru/relations

---
+++ rails-basics-ru/relations/db/migrate/20240406174941_add_user_and_status_references_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddUserAndStatusReferencesToTasks < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :tasks, :user, null: false, foreign_key: true
+    add_reference :tasks, :status, null: false, foreign_key: true
+  end
+end
